866a50f6e1d1426385acf297811e1b34f64feecc
itsGawr-Gura <itsgawrgura@gmail.com>
2024-04-22 17:24:36 +0300

    Добавьте файлы проекта.

---
+++ TotalCommander/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace TotalCommander
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.leftTW = new System.Windows.Forms.TreeView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.createDir = new System.Windows.Forms.ToolStripButton();
+            this.delete = new System.Windows.Forms.ToolStripButton();
+            this.rename = new System.Windows.Forms.ToolStripButton();
+            this.path = new System.Windows.Forms.TextBox();
+            this.path1 = new System.Windows.Forms.TextBox();
+            this.rightTW = new System.Windows.Forms.TreeView();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // leftTW
+            // 
+            this.leftTW.Location = new System.Drawing.Point(12, 226);
+            this.leftTW.Name = "leftTW";
+            this.leftTW.Size = new System.Drawing.Size(758, 831);
+            this.leftTW.TabIndex = 0;
+            this.leftTW.AfterLabelEdit += new System.Windows.Forms.NodeLabelEditEventHandler(this.leftTW_AfterLabelEdit);
+            this.leftTW.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.leftTW_AfterSelect);
+            this.leftTW.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.leftTW_NodeMouseDoubleClick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createDir,
+            this.delete,
+            this.rename});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1725, 42);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // createDir
+            // 
+            this.createDir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.createDir.Image = ((System.Drawing.Image)(resources.GetObject("createDir.Image")));
+            this.createDir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.createDir.Name = "createDir";
+            this.createDir.Size = new System.Drawing.Size(46, 36);
+            this.createDir.Text = "Создать";
+            this.createDir.Click += new System.EventHandler(this.CreateDir_Click);
+            // 
+            // delete
+            // 
+            this.delete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.delete.Image = ((System.Drawing.Image)(resources.GetObject("delete.Image")));
+            this.delete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(46, 36);
+            this.delete.Text = "Удалить";
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // rename
+            // 
+            this.rename.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.rename.Image = ((System.Drawing.Image)(resources.GetObject("rename.Image")));
+            this.rename.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.rename.Name = "rename";
+            this.rename.Size = new System.Drawing.Size(46, 36);
+            this.rename.Text = "Переименовать";
+            this.rename.Click += new System.EventHandler(this.rename_Click);
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(26, 177);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(654, 31);
+            this.path.TabIndex = 2;
+            // 
+            // path1
+            // 
+            this.path1.Location = new System.Drawing.Point(926, 177);
+            this.path1.Name = "path1";
+            this.path1.Size = new System.Drawing.Size(654, 31);
+            this.path1.TabIndex = 4;
+            // 
+            // rightTW
+            // 
+            this.rightTW.Location = new System.Drawing.Point(912, 226);
+            this.rightTW.Name = "rightTW";
+            this.rightTW.Size = new System.Drawing.Size(758, 831);
+            this.rightTW.TabIndex = 3;
+            this.rightTW.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.rightTW_AfterSelect);
+            this.rightTW.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.rightTW_NodeMouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1725, 1069);
+            this.Controls.Add(this.path1);
+            this.Controls.Add(this.rightTW);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.leftTW);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView leftTW;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton createDir;
+        private System.Windows.Forms.ToolStripButton delete;
+        private System.Windows.Forms.ToolStripButton rename;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.TextBox path1;
+        private System.Windows.Forms.TreeView rightTW;
+    }
+}
+
---
+++ TotalCommander/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TotalCommander
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDrives();
+            LoadDrives1();
+        }
+
+        private void LoadDrives()
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                TreeNode node = new TreeNode(drive.Name);
+                node.Tag = drive.RootDirectory;
+                leftTW.Nodes.Add(node);
+            }
+        }
+        private void LoadDrives1()
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                TreeNode node = new TreeNode(drive.Name);
+                node.Tag = drive.RootDirectory;
+                rightTW.Nodes.Add(node);
+            }
+        }
+        private void leftTW_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            
+            PopulateTreeView(e.Node, path);
+        }
+        private void PopulateTreeView(TreeNode parentNode, System.Windows.Forms.TextBox textBox)
+        {
+            try
+            {
+                DirectoryInfo directory = (DirectoryInfo)parentNode.Tag;
+                parentNode.Nodes.Clear();
+
+                foreach (var dir in directory.GetDirectories())
+                {
+                    TreeNode node = new TreeNode(dir.Name);
+                    node.Tag = dir;
+                    parentNode.Nodes.Add(node);
+                }
+
+                foreach (var file in directory.GetFiles())
+                {
+                    TreeNode node = new TreeNode(file.Name);
+                    node.Tag = file;
+                    parentNode.Nodes.Add(node);
+                }
+
+                textBox.Text = directory.FullName; 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void leftTW_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Node.Tag is DirectoryInfo)
+            {
+                PopulateTreeView(e.Node, path);
+            }
+            else if (e.Node.Tag is FileInfo)
+            {
+                
+                FileInfo file = (FileInfo)e.Node.Tag;
+                System.Diagnostics.Process.Start(file.FullName);
+            }
+        }
+
+        private void CreateDir_Click(object sender, EventArgs e)
+        {
+            TreeNode selectedNode = leftTW.SelectedNode;
+            if (selectedNode != null && selectedNode.Tag is DirectoryInfo)
+            {
+                string newDirPath = Path.Combine(((DirectoryInfo)selectedNode.Tag).FullName, "New Directory");
+                if (!Directory.Exists(newDirPath))
+                {
+                    Directory.CreateDirectory(newDirPath);
+                    PopulateTreeView(selectedNode, path);
+                }
+                else
+                {
+                    MessageBox.Show("Directory already exists.");
+                }
+            }
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            TreeNode selectedNode = leftTW.SelectedNode;
+            if (selectedNode != null)
+            {
+                if (selectedNode.Tag is DirectoryInfo)
+                {
+                    Directory.Delete(((DirectoryInfo)selectedNode.Tag).FullName, true);
+                }
+                else if (selectedNode.Tag is FileInfo)
+                {
+                    File.Delete(((FileInfo)selectedNode.Tag).FullName);
+                }
+                TreeNode parentNode = selectedNode.Parent;
+                selectedNode.Remove();
+                if (parentNode != null)
+                {
+                    PopulateTreeView(parentNode, path);
+                }
+            }
+        }
+
+        private void rename_Click(object sender, EventArgs e)
+        {
+            TreeNode selectedNode = leftTW.SelectedNode;
+            if (selectedNode != null)
+            {
+                leftTW.LabelEdit = true;
+                if (!selectedNode.IsEditing)
+                {
+                    selectedNode.BeginEdit();
+                }
+            }
+        }
+
+        private void leftTW_AfterLabelEdit(object sender, NodeLabelEditEventArgs e)
+        {
+            if (e.Node != null)
+            {
+                string newPath = Path.Combine(Path.GetDirectoryName(((FileSystemInfo)e.Node.Tag).FullName), e.Label);
+                try
+                {
+                    if (e.Node.Tag is DirectoryInfo)
+                    {
+                        Directory.Move(((DirectoryInfo)e.Node.Tag).FullName, newPath);
+                    }
+                    else if (e.Node.Tag is FileInfo)
+                    {
+                        File.Move(((FileInfo)e.Node.Tag).FullName, newPath);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                e.CancelEdit = true;
+                e.Node.EndEdit(false);
+                e.Node.Tag = new DirectoryInfo(newPath);
+            }
+        }
+
+        private void rightTW_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            PopulateTreeView(e.Node, path1);
+        }
+
+        private void rightTW_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Node.Tag is DirectoryInfo)
+            {
+                PopulateTreeView(e.Node, path);
+            }
+            else if (e.Node.Tag is FileInfo)
+            {
+
+                FileInfo file = (FileInfo)e.Node.Tag;
+                System.Diagnostics.Process.Start(file.FullName);
+            }
+        }
+    }
+}
